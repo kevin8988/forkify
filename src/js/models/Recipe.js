7e7cf517c1f2addbe37841f77f8b7d6de2beb140
KevinChips <kevsilva07@gmail.com>
2019-07-03 16:54:40 -0300

    add kg and g

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -52,6 +52,7 @@ export default class Recipe {
       "cup",
       "pound"
     ];
+    const units = [...unitsShort, "kg", "g"];
     const newIngredients = this.ingredients.map(element => {
       //1. Uniform units
       let ingredient = element.toLowerCase();
@@ -63,7 +64,7 @@ export default class Recipe {
 
       //3. Parse ingredients into count, unit and ingredient
       const arrIng = ingredient.split(" ");
-      const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+      const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
       let objIng;
       if (unitIndex > -1) {
